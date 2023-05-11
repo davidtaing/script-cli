de469268ea96c3c4f0ca92b499fced5f2fafd9cc
davidtaing <adavidtaing@gmail.com>
2023-05-11 20:44:01 +1000

    chore: remove comments

--- cmd/run.go
+++ cmd/run.go
@@ -119,13 +119,4 @@ func init() {
 	runCmd.AddCommand(runScriptCmd)
 	rootCmd.AddCommand(runCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// taskCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// taskCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
