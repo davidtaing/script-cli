3fd81aa9939925e10d16f3f5686bbd438df4d890
davidtaing <adavidtaing@gmail.com>
2023-05-17 18:08:26 +1000

    fix: broken import due to root module
    renaming

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/davidtaing/script-cli/cmd"
+import "github.com/davidtaing/scriptcli/cmd"
 
 func main() {
 	cmd.Execute()
